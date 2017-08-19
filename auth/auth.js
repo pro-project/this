c306ccc02b0039b1194740978b9700e52535ded4
dhrubesh <dhrubesh97@gmail.com>
2017-08-19 18:28:02 +0000

    added watson api

---
+++ auth/auth.js
@@ -0,0 +1,21 @@
+var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
+    var personality_insights = new PersonalityInsightsV3({
+    url: "https://gateway.watsonplatform.net/personality-insights/api",
+
+    // username: '{username}',
+     // password: '{password}',
+    version_date: '2016-10-20',
+    headers: {
+        'X-Watson-Learning-Opt-Out': 'true'
+    }
+    });
+    personality_insights.profile({
+          text: 'text goes here',
+          consumption_preferences: true
+          },
+          function (err, response) {
+            if (err)
+              console.log('error:', err);
+            else
+              console.log(JSON.stringify(response, null, 2));
+});
