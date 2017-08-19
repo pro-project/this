b3ca05ddfb65a67315fb4bdc921fea2d260d9133
dhrubesh <dhrubesh97@gmail.com>
2017-08-19 18:29:52 +0000

    added watson api

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express();
 const http = require('http').Server(app);
-
+const watson = require('watson-developer-cloud');
+var auth = require('./auth/auth.js');
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
@@ -11,4 +12,5 @@ app.get('/', function (req, res) {
 app.listen(8080, function(){
   // console.log(watson)
   console.log('listening on localhost..8080');
+  console.log('DO NOT FORGET TO COMMENT THE CREDENTIALS')
 });
